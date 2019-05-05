5c3788b04ebe06a311a1dbdd07045c7d98664d8a
BenjiBoy13 <ben99.planetatierra@gmail.com>
2019-05-05 09:55:52 -0500

    First commit

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+//DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+//App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'Your url');
+
+//Site name
+define('SITENAME', 'Your Sitename');
+
---
+++ app/views/includes/header.php
@@ -0,0 +1,8 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <title> <?php echo SITENAME; ?> </title>
+</head>
+<body>
